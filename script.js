6448fc1df8e99fd8bacab0e23e398f64093b5ae2
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-12-31 10:36:27 +0100

    Refactor code
    Rename addItem to onAddItemSubmit
    
    Refactor onAddItemSubmit functionality
    
    Create addItemToDOM functionality
    
    Create addItemToStorage functionality - stores list item to browser's local storage.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
 
-function addItem(e) {
+function onAddItemSubmit(e) {
     e.preventDefault()
 
     const newItem = itemInput.value
@@ -15,21 +15,47 @@ function addItem(e) {
         return
     }
 
-    // Create list item
-    const li = document.createElement('li')
-    li.appendChild(document.createTextNode(newItem))
+    // Create item DOM element
+    addItemToDOM(newItem)
 
-    const button = createButton('remove-item btn-link text-red')
-    li.appendChild(button)
-
-    // Add li item to list in the DOM
-    itemList.appendChild(li)
+    // Add item to local storage
+    addItemToStorage(newItem)
 
     // Check if items are present.
     checkUI()
 
     // Set input field to empty string
     itemInput.value = ''
+}
+
+function addItemToDOM(item) {
+    // Create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(item))
+
+    const button = createButton('remove-item btn-link text-red')
+    li.appendChild(button)
+
+    // Add li item to list in the DOM
+    itemList.appendChild(li)
+}
+
+// Add Item to Local Storage
+function addItemToStorage(item) {
+    let itemsFromStorage
+
+    // Check if item already exists in local storage
+    if (localStorage.getItem('items') == null) {
+        itemsFromStorage = []
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    }
+
+    // Add new item to array
+    itemsFromStorage.push(item)
+
+    // Convert to JSON string and store/set in local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
 // Create Button Function
@@ -99,7 +125,7 @@ function checkUI() {
 }
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem)
+itemForm.addEventListener('submit', onAddItemSubmit)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
 itemFilter.addEventListener('input', filterItems)
