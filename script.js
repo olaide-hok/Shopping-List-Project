873f4574f5caa4f95da8a38a024a2ba0152f9751
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-12-31 12:40:52 +0100

    Update and reset state
    Check for edit mode in onAddItemSubmit Fn.
    
    Update checkUI Fn to revert 'Update Item' btn back to 'Add Item' btn and sets the input field to an empty string.

--- script.js
+++ script.js
@@ -23,6 +23,15 @@ function onAddItemSubmit(e) {
         return
     }
 
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode')
+        removeItemFromStorage(itemToEdit.textContent)
+        itemToEdit.classList.remove('edit-mode')
+        itemToEdit.remove()
+        isEditMode = false
+    }
+
     // Create item DOM element
     addItemToDOM(newItem)
 
@@ -168,6 +177,8 @@ function filterItems(e) {
 
 // Remove filter and clear all button from UI when list items is empty
 function checkUI() {
+    itemInput.value = ''
+
     const items = itemList.querySelectorAll('li')
 
     if (items.length === 0) {
@@ -177,6 +188,9 @@ function checkUI() {
         clearBtn.style.display = 'block'
         itemFilter.style.display = 'block'
     }
+
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item'
+    formBtn.style.backgroundColor = '#333333'
 }
 
 // Initialize App
